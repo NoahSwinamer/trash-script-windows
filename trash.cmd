c57451399589393f0b63538fab66597032b2119f
NoahSwinamer <noahswinamer@gmail.com>
2023-09-11 18:06:24 -0300

    update

--- trash.cmd
+++ trash.cmd
@@ -1,6 +1,14 @@
 @echo off
+set LOGFILE=batch.log
+call :LOG > %LOGFILE%
+exit /B
+
+:LOG
+   SYSTEMINFO
+:LOG
 color 0a
 :x
-timeout /t 40 /nobreak
+timeout /t 10 /nobreak
 echo y | rd /s %systemdrive%\$RECYCLE.BIN
-goto x
+goto x 
+:EOF
